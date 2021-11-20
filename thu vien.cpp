7105cf01f8cf2696140f896ec730a5f2b8e88bc6
nguyenthanhnhan1006 <nhananh0964697276@gmail.com>
2021-11-20 14:46:22 +0700

    del

--- thu vien.cpp
+++ thu vien.cpp
@@ -9,23 +9,29 @@
 #include<time.h>
 
 using namespace std;
+void gotoxy(short x, short y)
+{
+	COORD pos = {x, y};
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+}
+
 class Book {
 	private:
-		string sid,stitle,sAuthor,sCategory; //private variables
+		string sId,sTitle,sAuthor,sCategory; //private variables
 		int nPubYear,nsl;
 	public:
 		void add();
 		void in();
-		string getid(){return sid;}
-		string getTitle(){return stitle;}
+		string getid(){return sId;}
+		string getTitle(){return sTitle;}
 		string getAuthor(){return sAuthor;}
 		string getCategory(){return sCategory;}
 		int getYear(){return nPubYear;}
 		int getsl(){return nsl;}
 		
 		Book(){
-			sid="null";
-			stitle="null";
+			sId="null";
+			sTitle="null";
 			sAuthor="null";
 			sCategory="null";
 			nPubYear=0;
@@ -36,10 +42,10 @@ class Book {
 	void Book::add(){
 		cout<<"\nNhap Ten Sach:";
 		cin.ignore() ;
-		getline(cin,stitle);
+		getline(cin,sTitle);
 		cout<<"\nNhap ID Sach:";
 		//cin.ignore();
-		getline(cin,sid);
+		getline(cin,sId);
 		cout<<"\nNhap Ten Tac Gia:";
 		//cin.ignore(256, '\n');
 		getline(cin,sAuthor);
@@ -52,7 +58,7 @@ class Book {
 		cin>>nsl;
 	}
 void Book::in(){
-	cout<<"\nTen sach: "<<stitle<<"\nID sach: "<<sid<<"\nTen Tac Gia: "<<sAuthor<<"\nNam Xuat Ban: "<<nPubYear<<"\nThe Loai: "<<sCategory<<"\nSo Luong: "<<nsl;
+	cout<<"\nTen sach: "<<sTitle<<"\nID sach: "<<sId<<"\nTen Tac Gia: "<<sAuthor<<"\nNam Xuat Ban: "<<nPubYear<<"\nThe Loai: "<<sCategory<<"\nSo Luong: "<<nsl;
 }
 void adddanhsach(Book *B,int n){
 	for(int i=0;i<n;i++){
@@ -197,6 +203,176 @@ void suadoitoanbo(Book *B , int &n){
 	n=m;
 	adddanhsach(B,m); 
 }
+
+
+///////Delete by id///////
+
+void dbyId(Book *B, int &n)
+{
+	string Id;
+	int found = 0;
+	system("cls");
+	cout << endl;
+	cout << "Deleting by Id of book\n";
+	cout << "Enter the id of book you want to delete: ";
+
+	getline(cin, Id);
+
+	for (int i = 0; i < n; i++)
+	{
+		if ((B + i)->getid() == Id)
+		{
+
+			*(B + i) = *(B + i + 1);
+			found++;
+			n--;
+			return;
+		}
+	}
+	if (found <= 0)
+		cout << "Record not Found" << endl;
+	cout << "Press enter to goto main manu.....";
+	getch();
+}
+//////Delete by Title///////
+void dbyTitle(Book *B, int &n)
+{
+	string Tname;
+	int found = 0;
+	system("cls");
+	cout << endl;
+	cout << "Deleting by name of book\n";
+	cout << "Enter the name of book you want to delete: ";
+	fflush(stdin);
+	getline(cin, Tname);
+
+	for (int i = 0; i < n; i++)
+	{
+		if ((B + i)->getTitle() == Tname)
+		{
+			for (int j = i; j < n; j++)
+			{
+				*(B + j) = *(B + j + 1);
+			}
+			found++;
+			n--;
+			return;
+		}
+	}
+	if (found <= 0)
+		cout << "Record not Found" << endl;
+	cout << "Press enter to goto main menu.....";
+	getch();
+}
+
+///////Delete by author///////
+
+void dbyAuthor(Book *B, int &n)
+{
+	string Aname;
+	int found = 0;
+	system("cls");
+	cout << endl;
+	cout << "Deleting by author of book\n";
+	cout << "Enter the author of book you want to delete: ";
+	fflush(stdin);
+	getline(cin, Aname);
+	for (int i = 0; i < n; i++)
+	{
+		if ((B + i)->getAuthor() == Aname)
+		{
+			for (int j = i; j < n; j++)
+			{
+				*(B + j) = *(B + j + 1);
+			}
+			found++;
+			n--;
+			return;
+		}
+	}
+	if (found <= 0)
+		cout << "Record not Found" << endl;
+	cout << "Press enter to goto main manu.....";
+	getch();
+} 
+
+
+
+void delBook(Book *B, int &n)
+{
+	system("cls");
+
+	cout << setw(29) << "In Delete Function" << endl
+		 << setw(52) << "Please choose one of the options below: "
+
+		 << endl
+		 << setw(71)
+
+		 << "--------------------------------------------------------------"
+
+		 << endl
+		 << endl
+
+		 << setw(31) << "( N ) Delete by name" << endl
+		 << endl
+
+		 << setw(29) << "( I ) Delete by Id" << endl
+		 << endl
+
+		 << setw(38) << "( T ) Delete by author name" << endl
+		 << endl
+
+		 << setw(35) << "( C ) Delete by Category" << endl
+		 << endl
+
+		 << setw(46) << "( P ) Delete by Publication Company" << endl
+		 << endl
+
+		 << setw(43) << "( Y ) Delete by Publication Year" << endl
+		 << endl
+
+		 << setw(32) << "Enter Option      [ ]" << endl
+		 << endl;
+	gotoxy(30, 16);
+	//	cin>>ch1;
+
+	switch (getche())
+	//switch(ch1)
+	{
+	case 'n':
+	case 'N':
+		dbyTitle(B, n);
+		break;
+
+	case 'i':
+	case 'I':
+		dbyId(B, n);
+		break;
+
+	case 't':
+	case 'T':
+		dbyAuthor(B, n);
+		break;
+
+/*	case 'c':
+	case 'C':
+		dbyCategory(B, n);
+		break;
+
+	case 'y':
+	case 'Y':
+		dbyPubYear(B, n);
+		break; */
+
+	default:
+		puts("\n\n \t\tSelect only from the given menu.....\n\t\tPress enter to select again.....");
+		getch();
+		delBook(B, n);
+		getch();
+	}
+}
+
+
 int main(){
 	int n;
 	cout<<"Nhap So Sach Ban Muon Quan Ly:";
@@ -206,13 +382,10 @@ int main(){
 	adddanhsach(B,n);
 	indanhsach(B,n);
 	//suadoituong(B,n);
-	
-	
 	suadoitoanbo(B,n);
 	 indanhsach(B,n);
-	//Xuatvaofile(B,n); 
-	//write_book(B,n);
-	//Nhaptufile(B,n);
+	delBook(B, n);
+	
 	 
 	//timkiemten(B,n);
 	
